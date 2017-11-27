035147ea5bbfaa3c21ad272662b9b7e4b3bf7911
dengguojian <dengguojian>
2017-11-27 13:55:01 +0800

    fix the network of max_pooling and add some other network structure (incomplete)

--- model_network.py
+++ model_network.py
@@ -1,16 +1,87 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
+import numpy as np
 import tensorflow.contrib.slim as slim
 
 def block(net, is_training, activation_fn=tf.nn.relu, scope=None, reuse=None):
     with tf.variable_scope(scope, 'min_block', [net], reuse=reuse):
         net = slim.conv2d(net, 32, [3, 3], stride=1, padding="SAME", scope='conv1')
-        net = slim.max_pool2d(net, [3, 3], stride=1, scope="maxpool1")
+        net = slim.max_pool2d(net, [3, 3], stride=3, scope="maxpool1")
         net = slim.batch_norm(net, is_training=is_training)
         if activation_fn:
             net = activation_fn(net)
     return net
+
+def convlutional_layer(data, kernel_size, bias_size, pooling_size):
+    kernel = tf.get_variable("conv", kernel_size, initializer=tf.random_normal_initializer())
+    bias = tf.get_variable("bias", bias_size, initializer=tf.random_normal_initializer())
+
+    conv = tf.nn.conv2d(data, kernel, strides=[1, 1, 1, 1], padding='VALID')
+    linear_output = tf.nn.relu(tf.add(conv, bias))
+    pooling = tf.nn.max_pool(linear_output, ksize=pooling_size, strides=pooling_size, padding="SAME")
+
+    return pooling
+
+def full_connection(data, weights_size, biases_size):
+    weights = tf.get_variable("weights", weights_size, initializer=tf.random_normal_initializer())
+    bias = tf.get_variable("bias", biases_size, initializer=tf.random_normal_initializer())
+
+    return tf.add(tf.matmul(data, weights), bias)
+
+def slim_convNet(data, keep_probability, phase_train, bottleneck_layer_size):
+    net = slim.conv2d(data, 32, [3, 3],stride=1, padding='SAME', scope='conv1')
+    net = tf.nn.relu(net)
+    net = slim.max_pool2d(net, [3, 3], stride=3, padding="SAME", scope="max_pool1")
+    net = slim.conv2d(net, 64, [3, 3], stride=1, padding='SAME', scope='conv2')
+    net = tf.nn.relu(net)
+    net = slim.max_pool2d(net, [3, 3], stride=3, padding="SAME", scope="max_pool2")
+
+    net = slim.flatten(net)
+    net = slim.dropout(net, keep_prob=keep_probability, is_training=phase_train, scope="Droput")
+    net = slim.fully_connected(net, 1024, activation_fn=tf.nn.relu, scope="full_connected")
+    net = slim.fully_connected(net, bottleneck_layer_size, activation_fn=None, scope="Bottleneck", reuse=False)
+    return net
+
+def original_convNet(data, image_size, keep_probability, bottleneck_layer_size):
+    kernel_shape1=[3, 3, 1, 32]
+    bias_shape1=[32]
+    pooling_size1 = [1, 2, 2, 1]
+
+    kernel_shape2=[3, 3, 32, 64]
+    bias_shape2=[64]
+    pooling_size2 = [1, 2, 2, 1]
+
+    flatten_size = np.ceil(image_size / 4)
+    full_conn_w_shape = [flatten_size * 64, 1024]
+    full_conn_b_shape = [1024]
+    out_w_shape = [1024, bottleneck_layer_size]
+
+    with tf.variable_scope("conv_layer_1") as layer1:
+        layer1_output = convlutional_layer(
+            data=data,
+            kernel_size=kernel_shape1,
+            bias_size=bias_shape1,
+            pooling_size=pooling_size1
+        )
+
+    with tf.variable_scope("conv_layer_1") as layer1:
+        layer2_output = convlutional_layer(
+            data=layer1_output,
+            kernel_size=kernel_shape2,
+            bias_size=bias_shape2,
+            pooling_size=pooling_size2
+        )
+
+def mini_facenet(data, keep_probability, phase_train, bottleneck_layer_size, reuse=None):
+    with tf.variable_scope("mini_facenet", reuse=reuse):
+        net = slim.repeat(data, 3, block, is_training=phase_train)
+        with tf.variable_scope("Logits"):
+            net = slim.flatten(net)
+            net = slim.dropout(net, keep_prob=keep_probability, is_training=phase_train, scope='Dropout')
+        net = slim.fully_connected(net, bottleneck_layer_size, activation_fn=None, scope='Bottleneck', reuse=False)
+    return net
+
 
 def inference(images, keep_probability, phase_train, bottleneck_layer_size, reuse=None):
     with tf.variable_scope("mini_facenet", reuse=reuse):
@@ -20,6 +91,9 @@ def inference(images, keep_probability, phase_train, bottleneck_layer_size, reus
             net = slim.dropout(net, keep_prob=keep_probability, is_training=phase_train, scope='Dropout')
         net = slim.fully_connected(net, bottleneck_layer_size, activation_fn=None, scope='Bottleneck', reuse=False)
     return net
+    # with slim.arg_scope([slim.conv2d, slim.fully_connected],
+    #                     weights_initializer=tf.truncated_normal_initializer(stddev=0.1)):
+    #     return slim_convNet(images, keep_probability, phase_train, bottleneck_layer_size)
 
 def triplet_loss(anchor, positive, negative, alpha):
     """
