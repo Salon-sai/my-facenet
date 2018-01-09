4ad8f1cad435bf07973b5214ef8c5947f27c1d7d
dengguojian <dengguojian>
2018-01-09 17:26:29 +0800

    temporary commit to add training age

--- train_properties_classifier.py
+++ train_properties_classifier.py
@@ -38,20 +38,54 @@ def gender_model(embeddings, weight_decay1, phase_train=True):
                 net = slim.fully_connected(net, num_outputs=2, activation_fn=None, scope="logits")
     return net
 
+def age_model(embeddings, weight_decay1, phase_train=True):
+    with tf.variable_scope("age_model"):
+        with slim.arg_scope([slim.fully_connected],
+                            weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
+                            biases_initializer=tf.constant_initializer(),
+                            # activation_fn=None,
+                            normalizer_fn=slim.batch_norm,
+                            normalizer_params={
+                                'decay': 0.995,
+                                'epsilon': 0.001,
+                                'updates_collections': None,
+                                'variables_collections': [tf.GraphKeys.TRAINABLE_VARIABLES],
+                            },
+                            weights_regularizer=slim.l1_regularizer(weight_decay1)):
+            with slim.arg_scope([slim.batch_norm], is_training=phase_train):
+                net = slim.fully_connected(embeddings, num_outputs=64, scope="hidden_1")
+                net = slim.fully_connected(net, num_outputs=32, scope="hidden_2")
+                net = slim.fully_connected(net, num_outputs=16, scope="hidden_3")
+                net = slim.fully_connected(net, num_outputs=8, scope="hidden_4")
+                net = slim.fully_connected(net, num_outputs=4, activation_fn=None, scope="logits")
+    return net
+
 def main(args):
     imdb_dir = os.path.expanduser(args.imdb_aligned_root)
     image_database = ImageDatabase(imdb_dir, args.max_num_images)
     batch_size = args.batch_size
+    now_datetime = datetime.datetime.now()
 
-    subdir = datetime.datetime.strftime(datetime.datetime.now(), 'gender-%Y%m%d-%H%M%S')
-    log_dir = os.path.join(os.path.expanduser(args.logs_base_dir), subdir)
-    model_dir = os.path.join(os.path.expanduser(args.models_base_dir), subdir)
+    gender_subdir = datetime.datetime.strftime(now_datetime, 'gender-%Y%m%d-%H%M%S')
+    gender_log_dir = os.path.join(os.path.expanduser(args.logs_base_dir), gender_subdir)
+    gender_model_dir = os.path.join(os.path.expanduser(args.models_base_dir), gender_subdir)
 
-    if not os.path.isdir(log_dir):
-        os.makedirs(log_dir)
+    age_subdir = datetime.datetime.strftime(now_datetime, 'age-%Y%m%d-%H%M%S')
+    age_log_dir = os.path.join(os.path.expanduser(args.logs_base_dir), age_subdir)
+    age_model_dir = os.path.join(os.path.expanduser(args.models_base_dir), age_subdir)
 
-    if not os.path.isdir(model_dir):
-        os.makedirs(model_dir)
+
+    if not os.path.isdir(gender_log_dir):
+        os.makedirs(gender_log_dir)
+
+    if not os.path.isdir(gender_model_dir):
+        os.makedirs(gender_model_dir)
+
+    if not os.path.isdir(age_log_dir):
+        os.makedirs(age_log_dir)
+
+    if not os.path.isdir(age_model_dir):
+        os.makedirs(age_model_dir)
 
 
     image_database.embeddings = calculate_embedding(facenet_model_path=args.model_path,
@@ -61,9 +95,13 @@ def main(args):
     embedding_size = image_database.embeddings.shape[1]
 
 
-    gender_classifier(embedding_size, args.weight_decay_l1, args.learning_rate, args.learning_rate_decay_step,
-                      args.learning_rate_decay_factor, args.optimizer, args.epoch_size, args.batch_size,
-                      log_dir, model_dir, subdir, image_database)
+    # gender_classifier(embedding_size, args.weight_decay_l1, args.learning_rate, args.learning_rate_decay_step,
+    #                   args.learning_rate_decay_factor, args.optimizer, args.epoch_size, args.batch_size,
+    #                   log_dir, model_dir, subdir, image_database)
+
+    age_classifier(embedding_size,  args.weight_decay_l1, args.learning_rate, args.learning_rate_decay_step,
+                   args.learning_rate_decay_factor, args.optimizer, args.epoch_size, args.batch_size,
+                   age_log_dir, age_model_dir, age_subdir, image_database)
 
 def gender_classifier(embedding_size, weight_decay_l1, learning_rate, learning_rate_decay_step,
                       learning_rate_decay_factor, optimizer_name, epoch_size, batch_size,
@@ -227,6 +265,127 @@ def save_variables_and_metagraph(session, saver, model_dir, model_name, global_s
         save_time_metagraph = time.time() - start_time
         print('Metagraph saved in %.2f seconds' % save_time_metagraph)
 
+def age_classifier(embedding_size, weight_decay_l1, learning_rate, learning_rate_decay_step,
+                   learning_rate_decay_factor, optimizer_name, epoch_size, batch_size,
+                   log_dir, model_dir, subdir, image_database):
+    _, train_embeddings, train_ages, _ = image_database.train_data
+    _, valid_embeddings, valid_ages, _ = image_database.valid_data
+    _, test_embeddings, test_ages, _ = image_database.test_data
+
+    with tf.Graph().as_default() as graph:
+        labels_placeholder = tf.placeholder(dtype=tf.int64, shape=[None], name="gender_label")
+
+        embeddings_placeholder = tf.placeholder(dtype=tf.float32, shape=[None, embedding_size],
+                                                name="embeddings_placeholder")
+
+        phase_train_placeholder = tf.placeholder(dtype=tf.bool, name="phase_gender_train")
+
+        global_step = tf.Variable(0, trainable=False)
+
+        logits = age_model(embeddings_placeholder, weight_decay_l1, phase_train_placeholder)
+
+        correct = tf.equal(tf.argmax(logits, 1), labels_placeholder)
+
+        correct_sum = tf.reduce_sum(tf.cast(correct, "float"))
+
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels_placeholder, logits=logits,
+                                                                       name="softmax_cross_entropy")
+        cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy_loss')
+
+        regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        total_losses = tf.add_n([cross_entropy_mean] + regularization_losses)
+
+        update_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "age_model")
+
+        learning_rate = tf.train.exponential_decay(learning_rate, global_step, learning_rate_decay_step,
+                                                   learning_rate_decay_factor, True)
+        tf.summary.scalar("learning_rate", learning_rate)
+
+        train_op = optimizer.train(total_loss=total_losses,
+                                   global_step=global_step,
+                                   optimizer=optimizer_name,
+                                   learning_rate=learning_rate,
+                                   moving_average_decay=0.99,
+                                   update_gradient_vars=update_vars,
+                                   log_historgrams=True)
+
+        saver = tf.train.Saver(update_vars, max_to_keep=3)
+
+        summary_op = tf.summary.merge_all()
+
+        summary_writer = tf.summary.FileWriter(log_dir, graph)
+
+        session = tf.Session(graph=graph)
+        session.run(tf.global_variables_initializer())
+        session.run(tf.local_variables_initializer())
+
+        epoch = 0
+        child_indexes = np.where(train_ages <= 18)[0]
+        youth_indexes = np.where(np.logical_and(train_ages > 18, train_ages <= 45))[0]
+        middle_indexes = np.where(np.logical_and(train_ages > 45, train_ages <= 59))[0]
+        old_indexes = np.where(train_ages > 59)[0]
+        while epoch < epoch_size:
+            gs = session.run(global_step, feed_dict=None)
+
+            selection_ages, selection_embeddings = ages_selection(train_embeddings,
+                                                                  child_indexes,
+                                                                  youth_indexes,
+                                                                  middle_indexes,
+                                                                  old_indexes,
+                                                                  max_num=1000)
+
+            train(session, selection_embeddings, selection_ages, embeddings_placeholder, labels_placeholder,
+                  phase_train_placeholder, global_step, total_losses, learning_rate, train_op, summary_op,
+                  summary_writer, batch_size)
+
+            print("saving the model parameters...")
+            save_variables_and_metagraph(session, saver, model_dir, subdir, gs)
+
+            print("evaluating...")
+            age_evaluate(session, valid_embeddings, valid_ages, embeddings_placeholder, labels_placeholder,
+                         phase_train_placeholder, gs, epoch, correct_sum, summary_writer)
+
+            epoch += 1
+
+        session.close()
+
+def ages_selection(embeddings, child_indexes, youth_indexes, middle_indexes, old_indexes, max_num=None):
+    np.random.shuffle(child_indexes)
+    np.random.shuffle(youth_indexes)
+    np.random.shuffle(middle_indexes)
+    np.random.shuffle(old_indexes)
+
+    if max_num is None:
+        num_image = min(len(child_indexes), len(youth_indexes), len(middle_indexes), len(old_indexes))
+    else:
+        num_image = min(len(child_indexes), len(youth_indexes), len(middle_indexes), len(old_indexes), max_num)
+
+    selection_ages = np.concatenate((np.full((num_image), 0),
+                    np.full((num_image), 1),
+                    np.full((num_image), 2),
+                    np.full((num_image), 3)))
+    selection_embeddings = np.concatenate((embeddings[child_indexes[:num_image]],
+                                           embeddings[youth_indexes[:num_image]],
+                                           embeddings[middle_indexes[:num_image]],
+                                           embeddings[old_indexes[:num_image]]), axis=0)
+
+    return selection_ages, selection_embeddings
+
+def age_evaluate(session, valid_embeddings, valid_ages, embeddings_placeholder, labels_placeholder,
+                 phase_train_placeholder, global_step, epoch, correct_sum, summary_writer):
+    summary = tf.Summary()
+
+    correct_count = session.run(correct_sum, feed_dict={
+        embeddings_placeholder: valid_embeddings,
+        labels_placeholder: valid_ages,
+        phase_train_placeholder: False
+    })
+
+    accuracy = float(correct_count) / float(len(valid_ages))
+
+    summary.value.add(tag="evaluate/accuracy", simple_value=accuracy)
+    summary_writer.add_summary(summary, global_step)
+    print("\t\t Epoch: %3d, Valid Accuracy: %1.4f" % (epoch, accuracy))
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
